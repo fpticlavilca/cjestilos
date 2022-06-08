cfffcc72649d8cae8e1c684d3c36e88edd752f87
fpticlavilca <99236280+fpticlavilca@users.noreply.github.com>
2022-06-08 12:49:46 -0500

    Update Person.php

--- controller/Person.php
+++ controller/Person.php
@@ -1,1 +1,19 @@
 <?php
+require_once "../model/ModelPerson.php"; 
+
+$request = $_REQUEST["request"];
+
+if($request == "stpCategoryC"){
+echo $request;
+}
+if($request == "stpCategoryR"){
+echo $request;
+}
+if($request == "stpCategoryU"){
+echo $request;
+}
+if($request == "stpCategoryD"{
+echo $request;
+}
+
+?>
